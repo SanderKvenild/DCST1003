b35ef3441ecef0a55e019b4869222c0a2d4e0ddc
Sander Kvenild <sanderkvenild@gmail.com>
2021-08-30 14:51:44 +0200

    Did oppgave 4

---
+++ js-ov2-sander-kvenild/4/oppgave4.js
@@ -0,0 +1,22 @@
+function parse() {
+    var input = document.getElementById("input"),
+        output = document.getElementById("output");
+
+    switch (input.value) {
+        case "H":
+            output.textContent = "Hjemme";
+            break;
+
+        case "U":
+            output.textContent = "Uavgjort";
+            break;
+
+        case "B":
+            output.textContent = "Borte";
+            break;
+    
+        default:
+            output.textContent = "Vennligst skriv in enten \"H\", \"U\", eller \"B\" i tekst feltet uten hermetegn (\")";
+            break;
+    }
+}
