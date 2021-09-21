980150d5c1474c972715ede74e66ab0e6b9ffefa
Sander Kvenild <sanderkvenild@gmail.com>
2021-09-21 15:44:47 +0200

    Improved grammar

--- js-ov03-sander-kvenild/5/oppgave5.js
+++ js-ov03-sander-kvenild/5/oppgave5.js
@@ -1,9 +1,15 @@
 var output = document.getElementById("output");
 
 for (var i = 10; i > 0; i--) {
-    output.innerHTML += i + " green bottles, hanging on a wall,<br>";
-    output.innerHTML += i + " green bottles, hanging on a wall,<br>";
-    output.innerHTML += "If 1 green bottle were to accidentally fall,<br>";
+    if (i==1) {
+        output.innerHTML += i + " green bottle, hanging on a wall,<br>";
+        output.innerHTML += i + " green bottle, hanging on a wall,<br>";
+    } else {
+        // TODO: Rewrite as continue
+        output.innerHTML += i + " green bottles, hanging on a wall,<br>";
+        output.innerHTML += i + " green bottles, hanging on a wall,<br>";
+    }
+        output.innerHTML += "If 1 green bottle were to accidentally fall,<br>";
     if (i==2) {
         "There'd be 1 green bottle, hanging on the wall,<br>";
         continue;
