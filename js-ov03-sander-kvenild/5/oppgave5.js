2a0c6f6efeec305552795d2df015481eee5a8cb1
Sander Kvenild <sanderkvenild@gmail.com>
2021-09-21 15:38:28 +0200

    Finished oppgave 5

--- js-ov03-sander-kvenild/5/oppgave5.js
+++ js-ov03-sander-kvenild/5/oppgave5.js
@@ -2,4 +2,11 @@ var output = document.getElementById("output");
 
 for (var i = 10; i > 0; i--) {
     output.innerHTML += i + " green bottles, hanging on a wall,<br>";
+    output.innerHTML += i + " green bottles, hanging on a wall,<br>";
+    output.innerHTML += "If 1 green bottle were to accidentally fall,<br>";
+    if (i==2) {
+        "There'd be 1 green bottle, hanging on the wall,<br>";
+        continue;
+    }
+    output.innerHTML += "There'd be " + (i-1) + " green bottles, hanging on the wall,<br>";
 }
