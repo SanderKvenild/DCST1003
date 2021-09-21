74ff3a593fe64e1457ee81d0bfe61a1a89ccdd7f
Sander Kvenild <sanderkvenild@gmail.com>
2021-09-21 13:30:29 +0200

    Added missing semicolon

--- js-ov03-sander-kvenild/1/oppgave1.js
+++ js-ov03-sander-kvenild/1/oppgave1.js
@@ -1,7 +1,7 @@
 var output = document.getElementById("output"),
     outString = "";
 
-var i = 1
+var i = 1;
 while (i <= 7) {
     outString += "Jeg er tall nummer " + i + "\n";
     i++;
--- js-ov03-sander-kvenild/2/oppgave2.js
+++ js-ov03-sander-kvenild/2/oppgave2.js
@@ -1,7 +1,7 @@
 var output = document.getElementById("output"),
     outString = "";
 
-var i = 12
+var i = 12;
 while (i <= 98) {
     outString += "Jeg er tall nummer " + i + "\n";
     i += 5;
