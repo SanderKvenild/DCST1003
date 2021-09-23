e31a7fed7dde69f988e9e12c39c4d2d9237b74b1
Sander Kvenild <sanderkvenild@gmail.com>
2021-09-23 20:24:01 +0200

    Treated <2 and floats Commented code.

--- js-ov03-sander-kvenild/10/oppgave10.js
+++ js-ov03-sander-kvenild/10/oppgave10.js
@@ -2,16 +2,24 @@ var input = document.getElementById("input");
 var output = document.getElementById("output");
 
 function primeCheck() {
-    // It is mathematically impossible for prime factors to excede the square root of the number
-    if (input.value == 1) {
-        output.innerHTML = "Ikke primtall";
+    // Treating special case "Numbers less than 2"
+    if (input.value < 2) {
+        output.innerHTML = input.value + "er ikke et primtall";
         return;
     }
+    // Treating special case "floats are not prime"
+    if (input.value - Math.floor(input.value)) {
+        output.innerHTML = input.value + " er ikke et primtall";
+        return;
+    }
+
+    // It is mathematically impossible for prime factors to excede the square root of the number
     for (i = 2; i <= Math.sqrt(input.value); i++) {
         if ( !(input.value % i) ) {
-            output.innerHTML = "Ikke primtall"
+            output.innerHTML = input.value + " er ikke et primtall"
             return;
         }
     }
-    output.innerHTML = "Primtall"
+
+    output.innerHTML = input.value + " er et primtall"
 }
