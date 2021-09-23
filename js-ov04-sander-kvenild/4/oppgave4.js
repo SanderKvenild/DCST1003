bc08f99b15cbc324d3d426dccdc5995103db082e
Sander Kvenild <sanderkvenild@gmail.com>
2021-09-23 22:10:40 +0200

    Made function for oppgave 4

---
+++ js-ov04-sander-kvenild/4/oppgave4.js
@@ -0,0 +1,16 @@
+var array = [];
+
+var output = document.getElementById("output"); // TODO: make a div for this id
+
+function call() {
+    // TODO: call other functions
+}
+
+function fillArray() {
+    array = [1, 2, 3, 4, 5, 6];
+}
+
+function readArrayToWebsite() {
+    // TODO: iterate over to add spaces
+    output.innerHTML = array;
+}
