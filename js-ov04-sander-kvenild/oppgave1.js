3f466a6207d0e6a713f5c16dde4524800bba9871
Sander Kvenild <sanderkvenild@gmail.com>
2021-09-24 10:15:21 +0200

    Added function to render output in HTML document

--- js-ov04-sander-kvenild/oppgave1.js
+++ js-ov04-sander-kvenild/oppgave1.js
@@ -1,6 +1,11 @@
 function teenager(age) {
-    if (age > 13 && age < 20) {
+    if (Number(age) > 13 && Number(age) < 20) {
         return "Du er tenåring";
     }
     return "Du er ikke tenåring";
+}
+
+function teenCheck() {
+    age = document.getElementById("age").value;
+    document.getElementById("output1").innerHTML = teenager(age)
 }
