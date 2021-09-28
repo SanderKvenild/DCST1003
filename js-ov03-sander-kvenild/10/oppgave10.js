17d3e87dd1f412b1424bc826e1cd60dd34bed409
Sander Kvenild <sanderkvenild@gmail.com>
2021-09-28 13:36:01 +0200

    Fixed bug with very large primes

--- js-ov03-sander-kvenild/10/oppgave10.js
+++ js-ov03-sander-kvenild/10/oppgave10.js
@@ -2,7 +2,22 @@
 var input = document.getElementById("input");
 var output = document.getElementById("output");
 
+/**
+ * Checks if the number in the element with id="input" is prime.
+ * Prints the answer out in the element with id="output" in the form of a string.
+ */
 function primeCheck() {
+    /*  Checking for very large inputs
+        According to later code large numbers are always divisible by 2
+
+        Largest tested to work: 688846502588399
+        Smallest tested to not work: 32361122672259149
+    */
+    if ( input.value.length >= 15 ) {
+        output.innerHTML = "Dette tallet er for stort, og vil gi feil svar"
+        return;
+    }
+
     // Checking for only integers 2 and over
     if (
         input.value < 2 ||                      // false if over 2
