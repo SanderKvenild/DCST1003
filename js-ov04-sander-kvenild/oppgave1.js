6f41aa634fa872156ddb7d2db7a71bc47f1b3bea
Sander Kvenild <sanderkvenild@gmail.com>
2021-09-28 14:44:43 +0200

    Combined oppgave1.js and oppgave2.js

--- js-ov04-sander-kvenild/oppgave1.js
+++ js-ov04-sander-kvenild/oppgave1.js
@@ -9,3 +9,13 @@ function teenCheck() {
     var age = document.getElementById("age").value;
     document.getElementById("output1").innerHTML = teenager(age);
 }
+
+function larger(a, b) {
+    return Math.max(a, b);
+}
+
+function largerCheck() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    document.getElementById("output2").innerHTML = larger(num1, num2);
+}
--- js-ov04-sander-kvenild/oppgave2.js
+++
@@ -1,9 +0,0 @@
-function larger(a, b) {
-    return Math.max(a, b);
-}
-
-function largerCheck() {
-    var num1 = document.getElementById("num1").value;
-    var num2 = document.getElementById("num2").value;
-    document.getElementById("output2").innerHTML = larger(num1, num2);
-}
