ec38c24ab35c0de73bb7905df9d70599890bfc4f
Sander Kvenild <sanderkvenild@gmail.com>
2021-09-30 16:05:33 +0200

    Did oppgave 8

---
+++ js-ov04-sander-kvenild/oppgave8.js
@@ -0,0 +1,10 @@
+var bricks = [[2,3,8], [4,3,2], [2,4,7], [6,5,1]]
+var volumes = []
+
+var output = document.getElementById("output");
+
+bricks.forEach(element => {
+    volumes.push(element[0]*element[1]*element[2]);
+});
+
+output.innerHTML += Math.max(volumes[0], volumes[1], volumes[2], volumes[3]) + " cm.";
