0fb70d7fad8da503b43ac74a9102bf1ad8f30f9a
Sander Kvenild <sanderkvenild@gmail.com>
2021-10-05 14:47:25 +0200

    Coppied oppgave 2

---
+++ js-ov5-sander-kvenild/3/oppgave3.js
@@ -0,0 +1,21 @@
+var div = document.getElementById("inputs")
+for(i=1; i<=5; i++) {
+    div.innerHTML += "Vare " + i + ": <input type=\"text\"><br>"
+}
+
+var output = document.getElementById("output");
+var inputs = document.getElementsByTagName("input");
+
+function read() {
+    var array = [];
+    for (const input in inputs) {
+        if (Object.hasOwnProperty.call(inputs, input)) {
+            const element = inputs[input];
+            if (element.value == '') {
+                continue;
+            }
+            array.push(element.value);
+        }
+    }
+    output.innerHTML = array.join(", ")
+}
