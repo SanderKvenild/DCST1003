e3dd5a090a99406f0224823cfa8faf14db2fced8
Sander Kvenild <sanderkvenild@gmail.com>
2021-10-05 14:24:13 +0200

    Added <br> between inputs

--- js-ov5-sander-kvenild/2/oppgave2.js
+++ js-ov5-sander-kvenild/2/oppgave2.js
@@ -1,18 +1,16 @@
 var div = document.getElementById("inputs")
 for(i=1; i<=5; i++) {
-    div.innerHTML += "Vare " + i + ": <input type=\"text\">"
+    div.innerHTML += "Vare " + i + ": <input type=\"text\"><br>"
 }
+
 var output = document.getElementById("output");
 var inputs = document.getElementsByTagName("input");
 var array = [];
 
 function read() {
     for (const input in inputs) {
-        if (Object.hasOwnProperty.call(inputs, input)) {
-            const element = inputs[input];
-            array.push(element.value);
-        }
+        const element = inputs[input];
+        array.push(element.value);
     }
-    console.log(array);
     output.innerHTML = array.join(", ")
 }
