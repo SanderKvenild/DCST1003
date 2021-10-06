75973ab9c4aba0bdd5603cbc3d8f7599627ee5d0
Sander Kvenild <sanderkvenild@gmail.com>
2021-10-06 15:41:20 +0200

    Changed inputs to use <table>

--- js-ov5-sander-kvenild/3/oppgave3.js
+++ js-ov5-sander-kvenild/3/oppgave3.js
@@ -1,21 +1,29 @@
 var div = document.getElementById("inputs")
 for(i=1; i<=5; i++) {
-    div.innerHTML += "Vare " + i + ": <input type=\"text\"><br>"
+    div.innerHTML += "<tr><td><input type=\"text\"></td><td><input type=\"number\"></td></tr>"
 }
 
 var output = document.getElementById("output");
 var inputs = document.getElementsByTagName("input");
 
 function read() {
-    var array = [];
-    for (const input in inputs) {
-        if (Object.hasOwnProperty.call(inputs, input)) {
-            const element = inputs[input];
+    var wares = [];
+    var prices = [];
+
+    for (const index in inputs) {
+        if (Object.hasOwnProperty.call(inputs, index)) {
+            const element = inputs[index];
             if (element.value == '') {
                 continue;
             }
-            array.push(element.value);
+
+            if (index % 2 == 0) {
+                wares.push(element.value);
+                continue;
+            }
+            prices.push(element.value);
         }
     }
-    output.innerHTML = array.join(", ")
+    output.innerHTML = "Varer: " + wares.join(", ") + "<br>" +
+    "Priser: " + prices.join(", ");
 }
