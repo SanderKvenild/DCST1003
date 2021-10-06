d5f4fe64f23ec3d692846680455ec418b9b0796f
Sander Kvenild <sanderkvenild@gmail.com>
2021-10-06 22:02:20 +0200

    Cleaned up code Commented code

--- js-ov5-sander-kvenild/3/oppgave3.js
+++ js-ov5-sander-kvenild/3/oppgave3.js
@@ -1,4 +1,3 @@
-// TODO: Clean this spaghetti code
 var div = document.getElementById("inputs")
 for(i=1; i<=5; i++) {
     div.innerHTML += "<tr><td><input type=\"text\"></td><td><input type=\"number\"></td></tr>"
@@ -8,9 +7,17 @@ var output = document.getElementById("output");
 var inputs = document.getElementsByTagName("input");
 
 function read() {
+    // Inputs
     var wares = [];
     var prices = [];
 
+    // Calculated values
+    var totalPrice = 0;
+    var maxPrice;
+    var minPrice;
+    var averagePrice;
+
+    // TODO: Do something about one empty and one filled cell in a row
     for (input of inputs) {
         // Treating empty cell
         if (input.value == '') {
@@ -26,21 +33,24 @@ function read() {
         // Pushing prices to array
         prices.push(Number(input.value));
     }
-    
-    output.innerHTML = "Varer: " + wares.join(", ") + "<br>" +
-    "Priser: " + prices.join(", ") + "<br>" + "<br>";
 
-    var maxPrice = Math.max(...prices); // ... is the array spread operator, which makes array elements separate parameters
-    var minPrice = Math.min(...prices);
-
-    var totalPrice = 0;
+    // Adding up prices
     prices.forEach(price => {
         totalPrice += price
     });
-    var averagePrice = mRound(totalPrice / prices.length, 2);  // TODO: Consider optimizing prices.length to 5, and requiring all rows to be filled by user
 
-    output.innerHTML += "Høyeste pris: " + maxPrice + "<br>" +
-        "Laveste pris: " + minPrice + "<br>" + "<br>" +
+    maxPrice = Math.max(...prices); // ... is the array spread operator, which makes array elements separate parameters
+    minPrice = Math.min(...prices);
+    averagePrice = mRound(totalPrice / prices.length, 2);  // TODO: Consider optimizing prices.length to 5, and requiring all rows to be filled by user
+
+    // Writing to document
+    output.innerHTML = 
+        "Varer: " + wares.join(", ") + "<br>" +
+        "Priser: " + prices.join(", ") + "<br>" + 
+        "<br>" +
+        "Høyeste pris: " + maxPrice + "<br>" +
+        "Laveste pris: " + minPrice + "<br>" + 
+        "<br>" +
         "Sum av priser: " + totalPrice + "<br>" +
         "Gjennomsnittspris: " + averagePrice;
 }
