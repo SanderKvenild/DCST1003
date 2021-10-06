089ddd782737eb5496848a020993845a7b1080de
Sander Kvenild <sanderkvenild@gmail.com>
2021-10-06 21:09:01 +0200

    Calculating sum and average price

--- js-ov5-sander-kvenild/3/oppgave3.js
+++ js-ov5-sander-kvenild/3/oppgave3.js
@@ -1,3 +1,4 @@
+// TODO: Clean this spaghetti code
 var div = document.getElementById("inputs")
 for(i=1; i<=5; i++) {
     div.innerHTML += "<tr><td><input type=\"text\"></td><td><input type=\"number\"></td></tr>"
@@ -21,15 +22,33 @@ function read() {
                 wares.push(element.value);
                 continue;
             }
-            prices.push(element.value);
+            prices.push(Number(element.value));
         }
     }
     output.innerHTML = "Varer: " + wares.join(", ") + "<br>" +
-    "Priser: " + prices.join(", ") + "<br>";
+    "Priser: " + prices.join(", ") + "<br>" + "<br>";
 
     var maxPrice = Math.max(...prices); // ... is the array spread operator, which makes array elements separate parameters
     var minPrice = Math.min(...prices);
-    
+
+    var totalPrice = 0;
+    prices.forEach(price => {
+        totalPrice += price
+    });
+    var averagePrice = mRound(totalPrice / prices.length, 2);  // TODO: Consider optimizing prices.length to 5, and requiring all rows to be filled by user
+
     output.innerHTML += "Høyeste pris: " + maxPrice + "<br>" +
-        "Laveste pris: " + minPrice;
+        "Laveste pris: " + minPrice + "<br>" + "<br>" +
+        "Sum av priser: " + totalPrice + "<br>" +
+        "Gjennomsnittspris: " + averagePrice;
+}
+
+/**
+ * Rounds to the amount of decimals given by the constant "accuracy"
+ * @param {number} float - To be rounded 
+ * @returns {number} Rounded - Number after rounding
+ */
+ function mRound(float, accuracy) {
+    // '+' removes trailing zeros
+    return +float.toFixed(accuracy);
 }
