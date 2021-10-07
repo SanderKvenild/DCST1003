83d769843c40482b8a8efdfbcd3b5af85846afe3
Sander Kvenild <sanderkvenild@gmail.com>
2021-10-07 12:42:30 +0200

    Commented code

--- js-ov5-sander-kvenild/5/oppgave5.js
+++ js-ov5-sander-kvenild/5/oppgave5.js
@@ -5,32 +5,41 @@ calculate();
 display();
 
 function calculate() {
+    // Looping rows
     for (var i = 1; i <= 10; i++) {
         var row = [];
+
+        // Looping columns
         for (var j = 1; j <= 10; j++) {
             row.push(mRound(i/j, 4));
         }
+
         DIVISJONStabellen.push(row);
     }
-    
-    console.log(DIVISJONStabellen);
 }
 
 function display() {
+    // Top left element
     outputText = "<tr><td><b>/</b></td>";
 
+    // Column labels
     for (var i = 1; i <= 10; i++) {
         outputText += "<td><b>" + i + "</b></td>";
     }
     outputText += "</tr>";
 
+    // Looping rows
     for (var i = 0; i < 10; i++) {
         outputText += "<tr><td><b>" + (i + 1) + "</b></td>";
+
+        // Looping columns
         for (var j = 0; j < 10; j++) {
             outputText += "<td>" + DIVISJONStabellen[i][j] + "</td>";
         }
         outputText += "</tr>";
     }
+
+    // Writing output to document
     output.innerHTML += outputText;
 }
 
