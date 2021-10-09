8be373dd3910d2968e5c2b24df6c6e5aa0367210
Sander Kvenild <sanderkvenild@gmail.com>
2021-10-09 15:04:31 +0200

    Did oppgave 7 Fixed typo in filename

---
+++ js-ov5-sander-kvenild/7/oppgave7.js
@@ -0,0 +1,17 @@
+calculateDivision();
+calculatePlus();
+
+var form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    var data = new FormData(form);
+    for (const entry of data) {
+        if (entry[1] == "plus") {
+            displayPlus();
+            return
+        }
+        displayDivision();
+    }
+}, false)
