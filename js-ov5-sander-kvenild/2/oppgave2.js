74444365586868772a8f3e8e0ea832d503f13c2b
Sander Kvenild <sanderkvenild@gmail.com>
2021-10-09 18:10:01 +0200

    Hardcoded input section

--- js-ov5-sander-kvenild/2/oppgave2.js
+++ js-ov5-sander-kvenild/2/oppgave2.js
@@ -1,9 +1,3 @@
-// TODO: Write this directly into HTML document
-var div = document.getElementById("inputs")
-for(i=1; i<=5; i++) {
-    div.innerHTML += "Vare " + i + ": <input type=\"text\"><br>"
-}
-
 var output = document.getElementById("output");
 var inputs = document.getElementsByTagName("input");
 
